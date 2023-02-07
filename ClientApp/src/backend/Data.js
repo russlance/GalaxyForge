895faa0bc1e2bbd51146d035243781bf631c3d79
rlance <russlance91@gmail.com>
2023-02-07 08:00:24 -0600

    Starting to add data management.

---
+++ ClientApp/src/backend/Data.js
@@ -0,0 +1,22 @@
+﻿import { createServer } from "miragejs";
+
+export const createServer = () => ({
+    routes() {
+        this.get("/api/files", () => [
+            {
+                id: 1,
+                fileName: "FILENAME+22+CHARAS1",
+                last_seen: "Wed Jan 25 2023 06:48:00",
+                file_status: "NOT_ON_FS",
+                signal_ids: [
+                    "<base>.eqdw",
+                    "<base>.eqdw",
+                    "<base>.eqdw",
+                    "<base>.eqdw",
+                ],
+                irod_ids: ["IROD1", "IROD2", "IROD3", "IROD4", "IROD5"],
+            },
+
+        ]);
+    },
+});
--- ClientApp/src/components/Home.jsx
+++ ClientApp/src/components/Home.jsx
@@ -1,52 +1,61 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { Box, Typography, Grid, Button } from "@mui/material";
 import { Info } from "./Info";
 import { MapComponent } from "./MapComponent";
 import RelationalGraph from "./RelationalGraph";
+import { response } from 'express';
 
-export class Home extends Component {
-    static displayName = Home.name;
-    constructor(props) {
-        super(props);
-    }
+//export class Home extends Component {
+export function Home() {
+    /**    static displayName = Home.name;
+        constructor(props) {
+            super(props);
+        } */
+    const [data, setData] = useState();
 
-    render() {
-        return (
+    useEffect(() => {
+        fetch("api/files")
+            .then((response) => response.json())
+            .then((json) => setData(json))
+    })
+
+    // render() {
+    return (
+        <Box
+            sx=
+            {{
+                display: "flex",
+                flexDirection: "row",
+                backgroundColor: "#212121",
+                height: "90vh",
+                justifyContent: "center",
+                p: "3px",
+            }}
+        >
             <Box
-                sx=
-                {{
-                    display: "flex",
-                    flexDirection: "row",
-                    backgroundColor: "#212121",
-                    height: "90vh",
-                    justifyContent: "center",
-                    p: "3px",
+                sx={{
+                    maxWidth: "60%",
                 }}
             >
-                <Box
-                    sx={{
-                        maxWidth: "60%",
-                    }}
-                >
-                    <MapComponent />
-                </Box>
-                <Box
-                    sx={{
-                        maxWidth: "40%",
-                    }}
-                >
-                    <Info
-                        sx={{
-                            height: "50vh",
-                        }}
-                    />
-                    <RelationalGraph
-                        sx={{
-                            height: "40vh",
-                        }}
-                    />
-                </Box>
+                <MapComponent />
             </Box>
-        );
-    }
+            <Box
+                sx={{
+                    maxWidth: "40%",
+                }}
+            >
+                <Info
+                    sx={{
+                        height: "50vh",
+                    }}
+                />
+                <RelationalGraph
+                    sx={{
+                        height: "40vh",
+                    }}
+                />
+            </Box>
+        </Box>
+    );
+}
 }
